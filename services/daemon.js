04373c30b658884392a5c7265be589d5f4c2bf82
Eason Chai <echai2905@gmail.com>
2021-08-11 15:30:52 +0800

    Add discord hook daemon

--- services/daemon.js
+++ services/daemon.js
@@ -2,30 +2,83 @@
 
 const fs = require("fs");
 const path = require("path");
+const axios = require("axios");
 const { printError } = require("../utils/helpers");
 const { setupInformation } = require("../utils/setup");
-const filename = ".secret.json";
+const { deviceInfo } = require("../utils/info");
+const secretFilename = ".secret.json";
+const systemFilename = "system.json";
+let interval;
+let secret;
+
+function saveToDisk(data) {
+  fs.writeFile(
+    path.resolve(`${__dirname}/../${systemFilename}`),
+    JSON.stringify(data),
+    { flag: "w" },
+    function (err) {
+      if (err) {
+        printError("", err, VERBOSE);
+      }
+    }
+  );
+}
+
+function getDataOnInterval(duration = 60000) {
+  interval = setInterval(async () => {
+    const data = await deviceInfo();
+    saveToDisk(data);
+
+    axios
+      .post(secret["discord"], {
+        content: JSON.stringify(data),
+        username: data.hostname,
+      })
+      .then(() => {
+        console.log("Update success");
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, duration);
+  return interval;
+}
+
+function killInterval(interval) {
+  clearInterval(interval);
+}
 
 async function runDaemon() {
-  let data;
-
   try {
-    data = fs.readFileSync(path.resolve(`./${filename}`), "utf8");
+    secret = JSON.parse(
+      fs.readFileSync(path.resolve(`./${secretFilename}`), "utf8")
+    );
   } catch (error) {
     if (error && error.code == "ENOENT") {
-      data = await setupInformation();
+      secret = JSON.parse(await setupInformation());
     } else if (error && error.code != "ENOENT") {
       printError("Error reading secret file...", error, true);
       process.exit(0);
     }
   }
 
-  if (data) {
-    const jsonData = JSON.parse(data);
-    if (jsonData["discord"]) {
-      console.log("discord");
+  getDataOnInterval();
+
+  if (secret) {
+    if (secret["discord"]) {
+      axios
+        .post(secret["discord"], {
+          content: "✅ Node System Monitor is online!",
+          username: "Node System Monitor",
+        })
+        .then(() => {
+          console.log("Update success");
+        })
+        .catch((err) => {
+          console.log(err);
+        });
     }
-    if (jsonData["telegram"]) {
+    if (secret["telegram"]) {
       console.log("telegram");
     }
   } else {
