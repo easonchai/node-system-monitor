2db177adff46e196140ba9b2512b2fb96d28bf5c
Eason Chai <echai2905@gmail.com>
2021-08-11 17:56:57 +0800

    Add new prompts

--- utils/prompts.js
+++ utils/prompts.js
@@ -1,4 +1,4 @@
-const { MultiSelect, Input } = require("enquirer");
+const { MultiSelect, Input, Select } = require("enquirer");
 
 const servicePrompt = new MultiSelect({
   name: "service",
@@ -20,8 +20,26 @@ const telegramTokenPrompt = new Input({
   initial: "6734...Js48",
 });
 
+const addOrEditPrompt = new Select({
+  message: "Would you like to add or edit an existing secret?",
+  choices: ["Add", "Edit"],
+});
+
+const addPrompt = (choices = new MultiSelect({
+  message: "What secrets do you want to add?",
+  choices,
+}));
+
+const editPrompt = (choices = new MultiSelect({
+  message: "Which secrets do you want to edit?",
+  choices,
+}));
+
 module.exports = {
   servicePrompt,
   discordUrlPrompt,
   telegramTokenPrompt,
+  addOrEditPrompt,
+  addPrompt,
+  editPrompt,
 };
