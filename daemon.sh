27536b5286c9c6f50c70db2f5c04de9d41f6d1d9
Eason Chai <echai2905@gmail.com>
2021-08-11 16:34:43 +0800

    Update daemon

--- daemon.sh
+++ daemon.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-if ! node -v > /dev/null
-then
-    /bin/bash ./dependency_setup.sh
-fi
-
 cp services/daemon.js /etc/systemd/system/nsm-daemon.js
 chmod +x /etc/systemd/system/nsm-daemon.js
 systemctl enable nsm-daemon
